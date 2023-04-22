246895e1321fb9bf03fe90de4c56c7530e2b7c3a
Ayush <Ayushsaxena38@gmail.com>
2023-04-22 19:05:04 +0530

    setup delete functionality for comments and authorized user can delete a comment or post

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -18,3 +18,28 @@ module.exports.addComment = function(req,res){
         })
     })
 }
+module.exports.delete = function(req,res){
+    console.log(req.params.id);
+    Comment.findById(req.params.id)
+    .then((cmnt)=>{
+        if(req.user.id == cmnt.user){
+            let postId=cmnt.post;
+            Comment.deleteOne(cmnt)
+            .then((err)=>{
+                console.log('error in deleting the comment err :',err);
+            })
+            Post.findByIdAndUpdate(postId, { $pull : {"comments": req.params.id}})
+            .then((p1)=>{
+                console.log('post is updated : ',p1);
+            })
+            //another way of updating the post
+            // Post.findById(postId)
+            // .then((post)=>{
+            //     post.comments.pull(req.params.id);
+            //     post.save();
+            // })
+            console.log('deleted');
+            res.redirect('back');
+        }
+    })
+}
--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const passport = require('passport');
 const commentController = require('../controllers/comment_controller');
 router.post('/addComment',passport.checkAuthenticationFeed,commentController.addComment)
+router.get('/delete/:id',passport.checkAuthentication,commentController.delete);
 
 module.exports = router;
